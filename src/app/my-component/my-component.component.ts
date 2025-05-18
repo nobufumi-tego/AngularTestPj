6a20faa41b52f650a2b3ab1a64eddd9d29bb8ff4
Nobufumi Yoshida <nobufumi.yoshida@tegosacloud.com>
2025-05-18 08:44:09 +0000

    20250518-gcp-console

--- src/app/my-component/my-component.component.ts
+++ src/app/my-component/my-component.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-component.component.scss']
 })
 export class MyComponentComponent {
+  message = 'ボタンを押すと現在の日時を表示します。';
 
+  changeMessage() {
+    const now = new Date();
+    const formatted = now.toLocaleString('ja-JP', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      weekday: 'long',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    });
+    this.message = `現在の日時：${formatted}`;
+  }
 }
